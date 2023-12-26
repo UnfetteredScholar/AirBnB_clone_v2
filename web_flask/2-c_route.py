940fe02f230dbeaf6fd22692df0f9fe066d61209
Ato Toffah <atotoffah@gmail.com>
2023-12-26 18:56:41 +0000

    2. C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Defines a Flask web app with routes
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.get("/", strict_slashes=False)
+def index():
+    """
+    Root endpoint of Flask application
+
+    Returns:
+        (str) Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.get("/hbnb", strict_slashes=False)
+def get_hbnb():
+    """
+    hbnb endpoint of Flask application
+
+    Returns:
+        HBNB
+    """
+    return "HBNB"
+
+
+@app.get("/c/<text>", strict_slashes=False)
+def get_c(text):
+    """
+    Returns C followed by the path parameter
+    """
+    return f"C {text}".replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
